780f36b48a3e9cf130e3d0755a58b38e8e24fc98
iamhaao <Haolee2002tst@gmail.com>
2023-05-11 14:18:00 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const  path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs  = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname,'public')));
+//HTTP logger
+app.use(morgan('combined'));
+
+app.engine('hbs', exphbs({
+  extname: '.hbs'
+})); 
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources\\views'));
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/tin-tuc', (req, res) => {
+  res.render('news');
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
